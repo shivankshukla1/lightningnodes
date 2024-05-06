5816bbe92d81e7e2ca3c6da80ed1f28e27684fc9
Shivankshukla1 <shivankshukla1729@gmail.com>
2024-05-06 22:27:04 +0530

    last commit

--- assignment/answer/answers/shivank/app/libs/firebase/server_apps/config.ts
+++ assignment/answer/answers/shivank/app/libs/firebase/server_apps/config.ts
@@ -1,35 +1,35 @@
-"use server";
-import firebase from 'firebase/app';
-import { initializeApp, getApp } from "firebase/app";
-import 'firebase/auth';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+// "use server";
+// import firebase from 'firebase/app';
+// import { initializeApp, getApp } from "firebase/app";
+// import 'firebase/auth';
+// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-// import firebase from 'firebase'
-require('firebase/auth')
-export const firebaseConfig = {
-    apiKey: "AIzaSyBWmC2tyAXLqePImH1Z6Ew6jDtpwSKa6xM",
-    authDomain: "lightningnodes-assignment.firebaseapp.com",
-    projectId: "lightningnodes-assignment",
-    storageBucket: "lightningnodes-assignment.appspot.com",
-    messagingSenderId: "754244979264",
-    appId: "1:754244979264:web:1c35935bda038c0e6f2da5",
-    measurementId: "G-53W4GG2E4G"
-};
+// // import firebase from 'firebase'
+// require('firebase/auth')
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyBWmC2tyAXLqePImH1Z6Ew6jDtpwSKa6xM",
+//     authDomain: "lightningnodes-assignment.firebaseapp.com",
+//     projectId: "lightningnodes-assignment",
+//     storageBucket: "lightningnodes-assignment.appspot.com",
+//     messagingSenderId: "754244979264",
+//     appId: "1:754244979264:web:1c35935bda038c0e6f2da5",
+//     measurementId: "G-53W4GG2E4G"
+// };
   
 
-
-// if (!firebase.apps.length) {
-//     // Initialize Firebase
-//     firebase.initializeApp(firebaseConfig);
-//   }
-  let firebaseApp;
-    try {
-    firebaseApp = getApp();
-    } catch (e) {
-    firebaseApp = initializeApp(firebaseConfig);
-    }
+// A
+// // if (!firebase.apps.length) {
+// //     // Initialize Firebase
+// //     firebase.initializeApp(firebaseConfig);
+// //   }
+//   let firebaseApp;
+//     try {
+//     firebaseApp = getApp();
+//     } catch (e) {
+//     firebaseApp = initializeApp(firebaseConfig);
+//     }
 
   
-  export const auth = firebase.auth();
+//   export const auth = firebase.auth();
   
-  export default firebase;
+//   export default firebase;
