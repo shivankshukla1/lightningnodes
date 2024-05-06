18698979c27b63eb1f0eaf77df6ef210917ed920
Shivankshukla1 <shivankshukla1729@gmail.com>
2024-05-06 21:56:50 +0530

    last commit

--- assignment/answer/answers/shivank/app/components/LoginModal.tsx
+++ assignment/answer/answers/shivank/app/components/LoginModal.tsx
@@ -181,12 +181,10 @@ export default function LoginModal() {
                 </h1>
                 <form className="space-y-4 md:space-y-6 "onSubmit={handleSignIn}>
                     <div>
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                         <Input type="email" name="email" id="email" classNameName="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" onChange={(e) => setEmail(e.target.value)}/>
                       
                     </div>
                     <div>
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                         <Input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""  onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                     <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
