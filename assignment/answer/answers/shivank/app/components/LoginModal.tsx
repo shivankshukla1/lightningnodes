651ee8a0fc976c2a368c4a1d6b4fd7cd46078d5b
Shivankshukla1 <shivankshukla1729@gmail.com>
2024-05-06 22:14:51 +0530

    last commit

--- assignment/answer/answers/shivank/app/components/LoginModal.tsx
+++ assignment/answer/answers/shivank/app/components/LoginModal.tsx
@@ -102,201 +102,105 @@
 // }
 
 
-// "use client"
-// import LoginBtn from "./client/LoginBtn";
-// import UserLogin from "./client/UserLogin"
-// import logo from "./logo.png";
-// import React from "react";
-// import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
-// import { log } from "console";
-// import Image from 'next/image'
-// import {Input} from "@nextui-org/react";
-// import { useState } from "react";
-// import { initializeApp } from "firebase/app";
-// import { getAuth, signInWithEmailAndPassword, UserCredential } from "firebase/auth";
-// // import { GetUserToken } from "@/app/libs/firebase/init";
-// import { CreateFirebaseSession } from "@/app/libs/firebase/server_apps/session_gen";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBWmC2tyAXLqePImH1Z6Ew6jDtpwSKa6xM",
-//   authDomain: "lightningnodes-assignment.firebaseapp.com",
-//   projectId: "lightningnodes-assignment",
-//   storageBucket: "lightningnodes-assignment.appspot.com",
-//   messagingSenderId: "754244979264",
-//   appId: "1:754244979264:web:1c35935bda038c0e6f2da5",
-//   measurementId: "G-53W4GG2E4G"
-// };
-
-
-// const firebaseApp = initializeApp(firebaseConfig);
-
-// const auth = getAuth(firebaseApp);
-
-
-
-
-// export default function LoginModal() {
-  
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     // try {
-//     //   signInWithEmailAndPassword(auth, email, password)
-//     //   .then((userCredential: UserCredential) => {
-//     //     // const User_Token = userCredential.user;
-//     //     // CreateFirebaseSession(User_Token);
-//     //   })
-//     //   .catch((err: { code: string, message: string }) => {
-//     //     console.log(err.code);
-//     //     console.log(err.message);
-//     //   });
-//     //   console.log("success");
-//     // } catch (error) {
-//     //   console.log(error);
-//     // }
-//   };
-
-  
-  
-//   return (
-//     <section className="bg-gray-50 dark:bg-gray-900 max-h-screen">
-//     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-//         <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-//         <Image
-//           className="w-8 h-8 mr-2"
-//           src={logo}
-//           alt="logo"
-//           width={30}
-//         />
-
-//         LightningNodes
-
-//         </a>
-//         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-//             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-//                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-//                     Sign in to your account
-//                 </h1>
-//                 <form className="space-y-4 md:space-y-6 "onSubmit={handleSignIn}>
-//                     <div>
-//                         <Input type="email" name="email" id="email" classNameName="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}/>
-                      
-//                     </div>
-//                     <div>
-//                         <Input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""  onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}/>
-//                     </div>
-//                     <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-
-//                     <div className="flex items-center justify-between">
-                        
-//                         <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                        
-//                     </div>
-//                 </form>
-//                 <LoginBtn />
-//             </div>
-//         </div>
-//     </div>
-//   </section>
-
-//   );
-// }
-
 "use client"
-import { useState } from 'react';
-import { Input } from "@nextui-org/react";
+import LoginBtn from "./client/LoginBtn";
+import UserLogin from "./client/UserLogin"
+import logo from "./logo.png";
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import { log } from "console";
+import Image from 'next/image'
+import {Input} from "@nextui-org/react";
+import { useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, UserCredential } from "firebase/auth";
-import Image from 'next/image';
-import logo from "./logo.png";
+// import { GetUserToken } from "@/app/libs/firebase/init";
 import { CreateFirebaseSession } from "@/app/libs/firebase/server_apps/session_gen";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyBWmC2tyAXLqePImH1Z6Ew6jDtpwSKa6xM",
+  authDomain: "lightningnodes-assignment.firebaseapp.com",
+  projectId: "lightningnodes-assignment",
+  storageBucket: "lightningnodes-assignment.appspot.com",
+  messagingSenderId: "754244979264",
+  appId: "1:754244979264:web:1c35935bda038c0e6f2da5",
+  measurementId: "G-53W4GG2E4G"
 };
 
+
 const firebaseApp = initializeApp(firebaseConfig);
+
 const auth = getAuth(firebaseApp);
 
+
+
+
 export default function LoginModal() {
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // try {
-    //   const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    //   const User_Token = userCredential.user;
-    //   CreateFirebaseSession(User_Token);
-    //   console.log("success");
-    // } catch (error) {
-    //   console.log(error);
-    // }
-  };
+  // const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   // try {
+  //   //   signInWithEmailAndPassword(auth, email, password)
+  //   //   .then((userCredential: UserCredential) => {
+  //   //     // const User_Token = userCredential.user;
+  //   //     // CreateFirebaseSession(User_Token);
+  //   //   })
+  //   //   .catch((err: { code: string, message: string }) => {
+  //   //     console.log(err.code);
+  //   //     console.log(err.message);
+  //   //   });
+  //   //   console.log("success");
+  //   // } catch (error) {
+  //   //   console.log(error);
+  //   // }
+  // };
 
+  
+  
   return (
     <section className="bg-gray-50 dark:bg-gray-900 max-h-screen">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <Image
-            className="w-8 h-8 mr-2"
-            src={logo}
-            alt="logo"
-            width={30}
-          />
-          LightningNodes
+        <Image
+          className="w-8 h-8 mr-2"
+          src={logo}
+          alt="logo"
+          width={30}
+        />
+
+        LightningNodes
+
         </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
-            </h1>
-            <form className="space-y-4 md:space-y-6" onSubmit={handleSignIn}>
-              <div>
-                <Input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-                  required=""
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-                />
-              </div>
-              <div>
-                <Input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Sign in
-              </button>
-              <div className="flex items-center justify-between">
-                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">
-                  Forgot password?
-                </a>
-              </div>
-            </form>
-          </div>
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Sign in to your account
+                </h1>
+                {/* <form className="space-y-4 md:space-y-6 "onSubmit={handleSignIn}>
+                    <div>
+                        <Input type="email" name="email" id="email" classNameName="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}/>
+                      
+                    </div>
+                    <div>
+                        <Input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""  onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}/>
+                    </div>
+                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+
+                    <div className="flex items-center justify-between">
+                        
+                        <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                        
+                    </div>
+                </form> */}
+                <LoginBtn />
+            </div>
         </div>
-      </div>
-    </section>
+    </div>
+  </section>
+
   );
 }
+
